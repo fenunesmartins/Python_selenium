7f46ad314079c915e7d2abfe4f09655ec43fee1f
Felipe <fenunesmartins8 9@gmail.com>
2021-08-13 21:15:09 -0300

    Adicionando os arquivos .py

---
+++ selenium_js_dinamico.py
@@ -0,0 +1,14 @@
+# 1. Pegar conteúdo HTML e partir da URL]
+# 2. Parsear o conteúdo HTML - BeautifulSoup
+# 3. Estruturar conteúdo em um Data Frame - Pandas
+# 4. Transformar os Dados em um Dicionário de dados próprio]
+# 5. Converter e salvar em um arquivo Json
+
+import time
+import requests
+import  pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import  Options
+
+
